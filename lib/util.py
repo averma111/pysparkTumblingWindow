bff04f3e3e477bc63db02663c17efdbde5556415
Your Name <ashish.gcp.verma@gmail.com>
2020-10-29 19:57:41 -0700

    Adding the lib package

---
+++ lib/util.py
@@ -0,0 +1,29 @@
+from pyspark import SparkConf
+import configparser
+
+
+def get_spark_app_config():
+    spark_conf = SparkConf()
+    config = configparser.ConfigParser()
+    config.read("config/spark.conf")
+    for (key, val) in config.items("SPARK_APP_CONFIGS"):
+        spark_conf.set(key, val)
+    return spark_conf
+
+
+def read_kafka_stream(spark):
+    return spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "invoices") \
+        .option("startingOffsets", "earliest") \
+        .option("includeHeaders", "true") \
+        .load()
+
+def write_output_console(output_df):
+    return  output_df.writeStream \
+        .format("console") \
+        .outputMode("update") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="1 minute") \
+        .start()
